ad269f50db0d0d3544391f7b23b6b8cfffd7b2bf
Senzo Masango <senzo@vxpress.co.za>
2026-06-29 14:38:40 +0200

    feat: setup project structure and routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
-import { Geist, Geist_Mono, Manrope } from "next/font/google"
+import {  Geist_Mono, Manrope } from "next/font/google"
 
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-provider"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { Toaster } from "@/components/ui/sonner"
+import { cn } from "@/lib/utils"
+import { metadata } from "@/lib/metadata"
 
-const manrope = Manrope({subsets:['latin'],variable:'--font-sans'})
+export { metadata }
+
+const manrope = Manrope({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,10 +25,18 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", manrope.variable)}
+      className={cn(
+        "antialiased selection:bg-primary",
+        fontMono.variable,
+        "font-sans",
+        manrope.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <TooltipProvider>{children}</TooltipProvider>
+          <Toaster theme="system" position="top-center" />
+        </ThemeProvider>
       </body>
     </html>
   )
